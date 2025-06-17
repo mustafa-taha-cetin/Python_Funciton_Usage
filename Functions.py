297da491cc2de85602625760431e41e614a335eb
MustafaTahaCetin <=>
2025-06-17 12:47:08 +0300

    codes

---
+++ Functions.py
@@ -0,0 +1,17 @@
+
+#------------------Functions----------------------------
+
+# def : fonksiyon oluşturmayı sağlar
+def my_function():
+    print("Mustafa is most intelligent people in this universe")
+    print(1,2,3,4,5)
+    print("Tuğçe is most beautiful girl in this world")
+
+my_function()
+
+
+# while : belirli bir koşul sağlanıyorken sürekli çalışmaya devam eder
+
+
+
+
